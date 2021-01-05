c535a501522325e40202f31bd38ae264af2a82ae
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2021-01-05 17:06:59 -0300

    Update gradle.groovy
    mod

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -15,39 +15,39 @@ def call(String seleccion){
     	     case 'Build_test':
     	         stage("Build_test") {
 					sh "gradle clean build"
-    	         }break
+    	      }; break
 				 
-			 case 'Sonar':
+	      case 'Sonar':
     	         stage("Sonar") {
-                def scannerHome = tool 'sonar';
-                withSonarQubeEnv('sonar-fsa') {
-                bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-					}
-				} break
-				
-			case 'Run':
-				 stage("Run") {
-					sh "gradle bootRun &"
-				} break
-				
-			case 'Sleep':
-				stage("Sleep") {
-					 sh 'sleep 200'
-				}break
-			
-			case 'Tes_Rest':
-				stage("Tes_Rest") {
-					sh "curl -X GET localhost:8085/rest/mscovid/test?msg=testing -O  && dir"
-				}break
-				
-			case 'uploadNexus':
+				def scannerHome = tool 'sonar';
+				withSonarQubeEnv('sonar-fsa') {
+				bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+				}
+		}; break
+
+		case 'Run':
+			 stage("Run") {
+				sh "gradle bootRun &"
+			} break
+
+		case 'Sleep':
+			stage("Sleep") {
+				 sh 'sleep 200'
+			}break
+
+		case 'Tes_Rest':
+			stage("Tes_Rest") {
+				sh "curl -X GET localhost:8085/rest/mscovid/test?msg=testing -O  && dir"
+			}break
+
+		case 'uploadNexus':
 			stage("uploadNexus") {
-                nexusPublisher nexusInstanceId: 'Nexus',
-                nexusRepositoryId: 'test-nexus',
-                packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Flavio\\.jenkins\\workspace\\job-nexus\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
-				} break
-			default:
-				println "Parametro ${item} incorrecto"
+			nexusPublisher nexusInstanceId: 'Nexus',
+			nexusRepositoryId: 'test-nexus',
+			packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Flavio\\.jenkins\\workspace\\job-nexus\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
+			} break
+		default:
+			println "Parametro ${item} incorrecto"
 			}
 		} 
 	catch(Exception e) {
