ad204f656e070a9ddb3c6a08f53f9bd52b178d3c
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2021-01-05 20:18:45 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,59 +1,41 @@
-def call(String seleccion){
-    println "Stages recibidos: ${seleccion}"
-	println "Stages recibidos: ${seleccion}"
-    
-	if (seleccion == '') {seleccion='Build_test;Sonar;Run;Sleep;Tes_Rest;uploadNexus'}
-    
-    String[] items = seleccion.toLowerCase().split(";")
-	
-    for (String item: items) {
-    	println "Stage en proceso: ${item}"
+def call(String stagename = 'all') {
 
-	try {
-    	   switch (item) {
-		   
-    	     case 'Build_test':
-    	         stage("Build_test") {
-					sh "gradle clean build"
-    	      }; break
-				 
-	      case 'Sonar':
-    	         stage("Sonar") {
-				def scannerHome = tool 'sonar';
-				withSonarQubeEnv('sonar-fsa') {
-				bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+    if (stagename == 'build' || stagename == 'test' || stagename == 'all') {
+        stage('build & test') {
+            env.JENKINS_STAGE = env.STAGE_NAME
+            sh "gradle clean build"
+        }
+    }
+
+    if (stagename == 'sonar' || stagename == 'all') {
+        stage('sonar') {
+            env.JENKINS_STAGE = env.STAGE_NAME
+            def scannerHome = tool 'sonar';
+            withSonarQubeEnv('sonar-fsa') { 
+                bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+            }
+        }
+    }
+    if (stagename == 'run' || stagename == 'all') {
+        stage('run') {
+            env.JENKINS_STAGE = env.STAGE_NAME
+            sh "gradle bootRun &"
+            sh 'sleep 200'
+        }
+    }
+    if (stagename == 'rest' || stagename == 'all') {
+        stage('rest') {
+            env.JENKINS_STAGE = env.STAGE_NAME
+            sh "curl -X GET localhost:8085/rest/mscovid/test?msg=testing -O  && dir"
+        }
+    }
+    if (stagename == 'nexus' || stagename == 'all') {
+        stage('nexus') {
+            env.JENKINS_STAGE = env.STAGE_NAME
+            nexusPublisher nexusInstanceId: 'Nexus',
+                nexusRepositoryId: 'test-nexus',
+                packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Flavio\\.jenkins\\workspace\\job-nexus\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
 				}
-		}; break
-
-		case 'Run':
-			 stage("Run") {
-				sh "gradle bootRun &"
-			}; break
-
-		case 'Sleep':
-			stage("Sleep") {
-				 sh 'sleep 200'
-			}; break
-
-		case 'Tes_Rest':
-			stage("Tes_Rest") {
-				sh "curl -X GET localhost:8085/rest/mscovid/test?msg=testing -O  && dir"
-			}; break
-
-		case 'uploadNexus':
-			stage("uploadNexus") {
-			nexusPublisher nexusInstanceId: 'Nexus',
-			nexusRepositoryId: 'test-nexus',
-			packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Flavio\\.jenkins\\workspace\\job-nexus\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
-			}; break
-		default:
-			println "Parametro ${item} incorrecto"
-			};
-		} 
-	catch(Exception e) {
-		println "Se produjo el error ${e}"}
-	}
+    }
 }
-
-return this;
-
+return this
