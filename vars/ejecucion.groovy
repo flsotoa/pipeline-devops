e443dabbb1b0f895dcc378ea814de8faefb9cf61
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2021-01-07 23:29:30 -0300

    Update ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -2,11 +2,15 @@ def call(){
 
     pipeline {
         agent any
-
+        parameters { 
+        choice(name: 'HERRAMIENTA', choices: ['Gradle'], description: '') 
+        
         stages {
             stage('pipeline') {
                 steps {
                     script {
+                        
+                        figlet params.HERRAMIENTA
                         
                         if(env.GIT_BRANCH=='develop' || env.GIT_BRANCH.contains('feature'))
                         {
