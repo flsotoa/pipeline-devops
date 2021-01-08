c8edb5b36455f4b0f2a6972d75ffd0cea5641c8b
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2021-01-07 23:19:29 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,41 +1,31 @@
-def call(String stagename = 'all') {
+def call() {
+    figlet 'Continuous-Integration'
 
-    if (stagename == 'build' || stagename == 'test' || stagename == 'all') {
-        stage('build & test') {
-            env.JENKINS_STAGE = env.STAGE_NAME
-            sh "gradle clean build"
-        }
+    stage('buildAndTest') {
+         sh "gradle clean build"
     }
 
-    if (stagename == 'sonar' || stagename == 'all') {
-        stage('sonar') {
-            env.JENKINS_STAGE = env.STAGE_NAME
+    stage('sonar') {
+        // Nombre extraido desde Jenkins > Global tool configuration > SonarQube Scanner
             def scannerHome = tool 'sonar';
             withSonarQubeEnv('sonar-fsa') { 
-                bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-            }
+            bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
         }
     }
-    if (stagename == 'run' || stagename == 'all') {
-        stage('run') {
-            env.JENKINS_STAGE = env.STAGE_NAME
+
+    stage('runJar') {
             sh "gradle bootRun &"
-            sh 'sleep 200'
-        }
+            sh 'sleep 100'
     }
-    if (stagename == 'rest' || stagename == 'all') {
-        stage('rest') {
-            env.JENKINS_STAGE = env.STAGE_NAME
-            sh "curl -X GET localhost:8085/rest/mscovid/test?msg=testing -O  && dir"
-        }
+
+    stage('rest') {
+        sh "curl -X GET localhost:8085/rest/mscovid/test?msg=testing -O  && dir"
     }
-    if (stagename == 'nexus' || stagename == 'all') {
-        stage('nexus') {
-            env.JENKINS_STAGE = env.STAGE_NAME
-            nexusPublisher nexusInstanceId: 'Nexus',
+
+    stage('nexusCI') {
+        nexusPublisher nexusInstanceId: 'NexusLocal',
+		nexusPublisher nexusInstanceId: 'Nexus',
                 nexusRepositoryId: 'test-nexus',
                 packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'C:\\Users\\Flavio\\.jenkins\\workspace\\job-nexus\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
-				}
-    }
-}
-return this
+	}
+} 
