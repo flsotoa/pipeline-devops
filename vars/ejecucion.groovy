416822d7587953351dd2118dd6f12145a660d9a0
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2021-01-07 23:56:14 -0300

    Update ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -2,22 +2,20 @@ def call(){
 
     pipeline {
         agent any
-        parameters { 
-        choice(name: 'HERRAMIENTA', choices: ['Gradle'], description: '') 
-        
+
         stages {
             stage('pipeline') {
                 steps {
                     script {
                         
-                        figlet params.HERRAMIENTA
+                        figlet 'Gradle'
                         
                         if(env.GIT_BRANCH=='develop' || env.GIT_BRANCH.contains('feature'))
                         {
                             gradle-ci.call();
                         } else if(env.GIT_BRANCH.contains('release')) {
                             gradle-cd.call();
-                        } else {
+                        } else { //nada
                         }
                     }
                 }
