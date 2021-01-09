93bf2b41caff6b7963e29ca659a131ddc9413f27
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2021-01-09 12:48:50 -0300

    Create ejecucion_old.groovy

---
+++ vars/ejecucion_old.groovy
@@ -0,0 +1,29 @@
+def call(){
+
+pipeline {
+    agent any
+
+    parameters { choice(name: 'herramienta', choices: ['gradle', 'maven'], description: '') }
+    stages {
+        stage('Pipeline') {
+            steps {
+                script {
+
+                         echo params.herramienta
+
+                        if (params.herramienta == 'gradle') {
+                        def ejecucion = load 'gradle.groovy'
+                        ejecucion.call()
+                        }
+                        else {
+                         def ejecucion = load 'maven.groovy'
+                        ejecucion.call()
+				}
+			}
+			}
+		}
+	   }
+	}	
+}
+
+return this;
